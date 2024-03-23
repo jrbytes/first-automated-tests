bf4d68a2c551cdecb17289104dd005d7f2e573fe
Junior Bytes <itamazor@gmail.com>
2024-03-23 20:23:47 -0300

    mock input

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit: '
+
 blogs = dict() # blog_name : Blog object
 
 def menu():
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items():
